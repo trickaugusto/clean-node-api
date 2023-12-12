2cbbaf659a1fb393402a848e1fee496d8d9e04e4
trickaugusto <trickaugusto1206@gmail.com>
2023-12-12 12:57:37 -0300

    ensure emailValidator calls validator with corret email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -18,12 +18,19 @@ describe('Email Validator', () => {
     expect(isEmailValid).toBe(true)
   })
 
-  test('Should return true if validator returns true', () => {
+  test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
 
     const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@gmail.com')
 
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@gmail.com')
+
+    expect(validator.email).toBe('any_email@gmail.com')
   })
 })
