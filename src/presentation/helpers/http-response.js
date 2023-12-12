3355728b67b16479d8b8dc9281d1590249cc3a5b
trickaugusto <trickaugusto1206@gmail.com>
2023-12-12 12:40:13 -0300

    move errors to a custom folder

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,26 +1,25 @@
-const UnauthorizedError = require('./unauthorized-error')
-const ServerError = require('./server-error')
+const { UnauthorizedError, ServerError } = require('../errors')
 
 module.exports = class HttpResponse {
-  static badRequest (error){
+  static badRequest (error) {
     return {
       statusCode: 400,
       body: error
-    }  
+    }
   }
 
   static serverError () {
     return {
       statusCode: 500,
-      body: new ServerError() 
-    } 
+      body: new ServerError()
+    }
   }
 
   static unauthorizedError () {
     return {
       statusCode: 401,
-      body: new UnauthorizedError() 
-    } 
+      body: new UnauthorizedError()
+    }
   }
 
   static ok (data) {
